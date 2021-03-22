8cfdbb7acf018a1a83167b174377d17ab2efc57d
alejandroninsausti <alejandroninsausti@gmail.com>
2021-03-22 11:14:55 -0300

    Add planet orbit

---
+++ DSVJ2-2021-INSAUSTI/Assets/Weeks/01/Solar System/Scripts/PlanetManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlanetManager : MonoBehaviour
+{
+    public Transform sun;
+    public Transform mercury;
+    public Transform venus;
+    public Transform earth;
+    public Transform mars;
+    public Transform jupiter;
+    public Transform saturn;
+    public Transform uranus;
+    public Transform neptune;
+
+    public float scaleMultiplier;
+
+    void Awake()
+    {
+        sun.localScale *= scaleMultiplier;
+        mercury.localPosition *= scaleMultiplier;
+        venus.position = new Vector3(mercury.localPosition.x * 1.86f, 0, 0);
+        earth.position = new Vector3(mercury.localPosition.x * 2.58f, 0, 0);
+        mars.position = new Vector3(mercury.localPosition.x * 3.93f, 0, 0);
+        jupiter.position = new Vector3(mercury.localPosition.x * 13.44f, 0, 0);
+        saturn.position = new Vector3(mercury.localPosition.x * 24.68f, 0, 0);
+        uranus.position = new Vector3(mercury.localPosition.x * 49.57f, 0, 0);
+        neptune.position = new Vector3(mercury.localPosition.x * 77.78f, 0, 0);
+    }
+}
---
+++ DSVJ2-2021-INSAUSTI/Assets/Weeks/01/Solar System/Scripts/PlanetRotation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour
+{
+
+    public Transform parent;
+    public float orbitTime;
+
+    const float speedMod = 1;
+    public float anglePerSecond;
+    public float radius;
+    public float angle;
+
+    private void Start()
+    {
+        radius = this.transform.localPosition.x;
+        anglePerSecond = 360 / orbitTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle += anglePerSecond * Time.deltaTime;
+        this.transform.localPosition = new Vector3(GetX(), 0, GetZ());
+    }
+
+    float GetX()
+    {
+        return radius * Mathf.Cos(angle);
+    }
+    float GetZ()
+    {
+        return radius * Mathf.Sin(angle);
+    }
+}
