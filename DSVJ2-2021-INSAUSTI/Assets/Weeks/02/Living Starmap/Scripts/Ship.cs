1ba7494c5327f09578bd1d534a91f12694962d41
alejandroninsausti <alejandroninsausti@gmail.com>
2021-04-07 23:39:03 -0300

    Add Ship

---
+++ DSVJ2-2021-INSAUSTI/Assets/Weeks/02/Living Starmap/Scripts/Ship.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace LivingStarmap
+{
+    public class Ship : MonoBehaviour
+    {
+        [SerializeField] float moveSpeedMod;
+        [SerializeField] float rotateSpeedMod;
+
+        void Update()
+        {
+            transform.localPosition += transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * moveSpeedMod;
+            transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeedMod, 0);
+        }
+    }
+}
